b08538011ffd118f6d2c9d5bc41a0f2eaa5764a2
Ekamjot Singh <68366451+iekamjotsingh@users.noreply.github.com>
2021-10-25 10:36:27 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const mountainRange = document.querySelector('.home-mountains img')
+const title = document.querySelector('h1')
+
+gsap.to(mountainRange,{
+    y:-100,
+    scrollTrigger:{
+        trigger:'home-mountains',
+        scrub:true
+    }
+})
+
+gsap.to(title,{
+    y:500,
+    scrollTrigger:{
+        trigger:"home-mountains",
+        scrub:true
+    }
+})
+
+ScrollTrigger.create({
+    trigger:".animate-it-bitch",
+    pin:true,
+    markers:true,
+    end:"bottom 30%"
+})
